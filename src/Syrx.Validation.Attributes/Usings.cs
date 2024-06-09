84a0d97fbf10054a970bf2499b55596e677ed18e
David Sexton <sextondjc@users.noreply.github.com>
2024-06-09 18:23:10 +0100

    Sledgehammer
    Kill 'em all.

--- src/Syrx.Validation.Attributes/Usings.cs
+++
@@ -1,9 +0,0 @@
-﻿global using System;
-global using System.Collections;
-global using System.Collections.Generic;
-global using System.ComponentModel.DataAnnotations;
-global using System.Linq;
-global using System.Text;
-global using static Syrx.Validation.Attributes.RequiredDateOptions;
-global using static Syrx.Validation.Contract;
-global using static System.ComponentModel.DataAnnotations.Validator;
